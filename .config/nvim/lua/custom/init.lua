d94778775d493477059e2016f7089538f087a3a0
leocrossman <38471886+leocrossman@users.noreply.github.com>
2023-01-22 16:48:56 -0500

    initial config now 100% in lua. just need to port over the remaining config/plugins

--- .config/nvim/lua/custom/init.lua
+++ .config/nvim/lua/custom/init.lua
@@ -1,1 +1,2 @@
+require('custom.set')
 require('custom.remap')
---
+++ .config/nvim/lua/custom/packer.lua
@@ -0,0 +1,69 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	-- My plugins here
+	-- use 'foo1/bar1.nvim'
+	-- use 'foo2/bar2.nvim'
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	use ({
+		'luisiacc/gruvbox-baby',
+		as = 'gruvbox-baby',
+		config = function()
+			vim.cmd('colorscheme gruvbox-baby')
+		end
+	})
+
+	use ('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate'})
+	use 'nvim-treesitter/playground'
+
+	use 'theprimeagen/harpoon'
+
+	use 'mbbill/undotree'
+
+	use 'tpope/vim-fugitive'
+
+	use {
+		'VonHeikemen/lsp-zero.nvim',
+		branch = 'v1.x',
+		requires = {
+			-- LSP Support
+			{'neovim/nvim-lspconfig'},             -- Required
+			{'williamboman/mason.nvim'},           -- Optional
+			{'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+			-- Autocompletion
+			{'hrsh7th/nvim-cmp'},         -- Required
+			{'hrsh7th/cmp-nvim-lsp'},     -- Required
+			{'hrsh7th/cmp-buffer'},       -- Optional
+			{'hrsh7th/cmp-path'},         -- Optional
+			{'saadparwaiz1/cmp_luasnip'}, -- Optional
+			{'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+			-- Snippets
+			{'L3MON4D3/LuaSnip'},             -- Required
+			{'rafamadriz/friendly-snippets'}, -- Optional
+		}
+	}
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+end)
