2b56726bfbcf6825e89ad675609c90dd0f7122f5
leocrossman <38471886+leocrossman@users.noreply.github.com>
2023-05-31 23:04:20 -0400

    tmux sessions + fzf

--- .config/nvim/after/plugin/treesitter.lua
+++ .config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "rust", "go", "c", "lua", "vim" },
+  ensure_installed = { "javascript", "typescript", "rust", "go", "c", "lua", "vim", "http" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
--- .config/nvim/lua/custom/packer.lua
+++ .config/nvim/lua/custom/packer.lua
@@ -102,6 +102,56 @@ return require('packer').startup(function(use)
 
   use 'chrisbra/csv.vim'
 
+  use {
+    "cuducos/yaml.nvim",
+    ft = { "yaml" }, -- optional
+    requires = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope.nvim" -- optional
+    },
+  }
+
+  use {
+    "rest-nvim/rest.nvim",
+    requires = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("rest-nvim").setup({
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = false,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Encode URL before making request
+        encode_url = true,
+        -- Highlight request on run
+        highlight = {
+          enabled = true,
+          timeout = 150,
+        },
+        result = {
+          -- toggle showing URL, HTTP info, headers at top the of result window
+          show_url = true,
+          show_http_info = true,
+          show_headers = true,
+          -- executables or functions for formatting response body [optional]
+          -- set them to false if you want to disable them
+          formatters = {
+            json = "jq",
+            html = function(body)
+              return vim.fn.system({ "tidy", "-i", "-q", "-" }, body)
+            end
+          },
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        env_file = '.env',
+        custom_dynamic_variables = {},
+        yank_dry_run = true,
+      })
+    end
+  }
+
   -- Rust
   use 'simrat39/rust-tools.nvim' -- also 'neovim/nvim-lspconfig'
   use 'mfussenegger/nvim-dap'    -- debugging - also plenary.nvim
